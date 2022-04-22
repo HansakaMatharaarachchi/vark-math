9777b0296b1cc880edf2207838a11ddce0f15457
hansakakavinda <m.a.hansaka@gmail.com>
2022-04-22 20:49:55 +0530

    updated Question.cs

---
+++ Assets/Fabian/_Scripts/visual/Extra/AIQuizManager.cs
@@ -0,0 +1,77 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public struct AIQuestion
+{
+    public string question;
+    public List<string> options;
+    public int correctAnswer;
+}
+
+public class AIQuizManager : MonoBehaviour
+{
+    [SerializeField] private List<AIQuestion> parentsQuestionnaire = new List<AIQuestion>();
+    [SerializeField] private GameObject envContainer;
+    [SerializeField] private GameObject questionPanel;
+    [SerializeField] private GameObject endPanel;
+
+    [SerializeField] private TMP_Text questionTxt;
+    [SerializeField] private Button[] choices;
+
+    private int selectedQuestionIndex;
+    private int correctAnswersCount;
+    
+    public void StartQuiz()
+    {
+        PlayAuditoryQuestion();
+    }
+    
+    private async void PlayAuditoryQuestion()
+    {
+        envContainer.SetActive(true);
+        await Task.Delay(10000);
+        envContainer.SetActive(false);
+        questionPanel.SetActive(true);
+        DisplayQuestion();
+        foreach (Button button in choices)
+        {
+            button.onClick.AddListener(delegate { CheckAnswer(button); });
+        }
+    }
+
+    private void DisplayQuestion()
+    {
+        if (parentsQuestionnaire.Count > selectedQuestionIndex)
+        {
+            questionTxt.text = parentsQuestionnaire[selectedQuestionIndex].question;
+            for (int i = 0; i < choices.Length; i++)
+            {
+                choices[i].GetComponentInChildren<TMP_Text>().text = parentsQuestionnaire[selectedQuestionIndex].options[i];
+            }
+        }
+        else
+        {
+            questionPanel.SetActive(false);
+            endPanel.SetActive(true);
+            Debug.Log(correctAnswersCount + " " +selectedQuestionIndex);
+        }
+    }
+    
+    void CheckAnswer(Button btn)
+    {
+        if (btn.GetComponentInChildren<TMP_Text>().text == parentsQuestionnaire[selectedQuestionIndex].options[parentsQuestionnaire[selectedQuestionIndex].correctAnswer - 1])
+        {
+            ++ correctAnswersCount;
+        }
+        else
+        {
+            Debug.Log("YA STUPID");
+        }
+        ++ selectedQuestionIndex;
+        DisplayQuestion();
+    }
+}
--- Assets/_Scripts/Question.cs
+++ Assets/_Scripts/Question.cs
@@ -1,13 +1,9 @@
-using System;
 using _Scripts;
-using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public abstract class Question : MonoBehaviour
 {
-    [Header("Question Base")] [SerializeField]
-    private InGameUIManager inGameUIManager;
+    [Header("Question Base")] private InGameUIManager inGameUIManager;
 
     private void Awake()
     {
@@ -16,21 +12,25 @@ public abstract class Question : MonoBehaviour
 
     protected virtual void SetAnswerCorrect()
     {
-        GameManager.Instance.currentLevelProgress.noOfCorrectAnswers ++;
-        if (GameManager.Instance.currentLevelProgress.noOfCorrectAnswers == GameManager.Instance.currentLevelQuestions.Length)
+        GameManager.Instance.currentLevelProgress.noOfCorrectAnswers++;
+        if (GameManager.Instance.currentLevelProgress.noOfCorrectAnswers ==
+            GameManager.Instance.currentLevelQuestions.Length)
         {
             GameManager.Instance.SaveLastAttemptInCurrentLvl(true);
         }
+
         inGameUIManager.IsQuestionCorrect(true);
     }
 
     protected virtual void SetAnswerWrong()
     {
         GameManager.Instance.currentLevelProgress.noOfWrongAnswers++;
-        if (GameManager.Instance.currentLevelProgress.noOfWrongAnswers == GameManager.Instance.currentLevelQuestions.Length)
+        if (GameManager.Instance.currentLevelProgress.noOfWrongAnswers ==
+            GameManager.Instance.currentLevelQuestions.Length)
         {
             GameManager.Instance.SaveLastAttemptInCurrentLvl(false);
         }
+
         inGameUIManager.IsQuestionCorrect(false);
     }
 }
