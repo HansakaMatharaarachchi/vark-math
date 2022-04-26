d05ee6599ea67e6297f3f5d615a1f85751a55b44
koofizhubNavodya <86097290+koofizhubNavodya@users.noreply.github.com>
2022-04-27 00:16:41 +0530

    added the player prefs for kins scneces

--- Assets/AR Scripts/ClickBtn4.cs
+++ Assets/AR Scripts/ClickBtn4.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using _Scripts;
+public class ClickBtn4 : MonoBehaviour
+{//kins 2 scene
 
-public class ClickBtn4 : Question
-{
-
-
-
-    public Text evaluate;
+ public Text evaluate;
+    
      
 
     public Button[] answerBtnArr;
@@ -23,28 +22,33 @@ public class ClickBtn4 : Question
     {
 
 
-        answerBtnArr[0].GetComponentInChildren<Text>().text = "blue";
+        answerBtnArr[0].GetComponentInChildren<Text>().text = "red";
 
-        answerBtnArr[1].GetComponentInChildren<Text>().text = "yellow";
+        answerBtnArr[1].GetComponentInChildren<Text>().text = "green";
          
         //give random values for question and answers
 
 
-        answerBtnArr[0].onClick.AddListener(CheckAnswerCorrect);
         answerBtnArr[1].onClick.AddListener(CheckAnswerCorrect);
+        answerBtnArr[0].onClick.AddListener(CheckAnswerRight);
         
-         
-        answerBtnArr[0].onClick.AddListener(SetAnswerWrong);
-        answerBtnArr[1].onClick.AddListener(SetAnswerCorrect);
+    /*     
+        answerBtnArr[1].onClick.AddListener(SetAnswerWrong);
+        answerBtnArr[0].onClick.AddListener(SetAnswerCorrect);*/
 
     }
     public void CheckAnswerCorrect()
     {
 
         evaluate.text = "wrong answer";
+        GameManager.Instance.LoadNextScene();
+
     }
     public void CheckAnswerRight()
     {
         evaluate.text = "correct answer";
+        PlayerPrefs.SetFloat("CK", (PlayerPrefs.GetFloat("CK") + 1));
+        PlayerPrefs.Save();
+        GameManager.Instance.LoadNextScene();
     }
 }
