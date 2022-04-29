231656612b91dbcf1632ee00e5bedafec463ee15
hansakakavinda <m.a.hansaka@gmail.com>
2022-04-29 09:19:58 +0530

    lobby UI improvements

--- Assets/_Scripts/LevelManager.cs
+++ Assets/_Scripts/LevelManager.cs
@@ -11,13 +11,13 @@ namespace _Scripts
         {
             levels.Add(1, new Dictionary<LearningStyle, int[]>
             {
-                {LearningStyle.Visual, new[] {3, 4, 5}},
+                {LearningStyle.Visual, new[] {12, 13, 14}},
                 {LearningStyle.Auditory, new[] {4, 5, 6}},
                 {LearningStyle.Kinesthetic, new[] {7, 8, 9}},
             });
             levels.Add(2, new Dictionary<LearningStyle, int[]>
             {
-                {LearningStyle.Visual, new[] {6, 7, 8}},
+                {LearningStyle.Visual, new[] {15, 16, 17}},
                 {LearningStyle.Auditory, new[] {7, 8, 9}},
                 {LearningStyle.Kinesthetic, new[] {7, 8, 9}},
             });
